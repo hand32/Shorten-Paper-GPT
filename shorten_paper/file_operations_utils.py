25e75774f81a25f8ff06dd52eb189262826c632a
Han DongHeun <rudnfehdgus@gmail.com>
2023-05-03 18:53:43 +0900

    fix: Handle All Encoding Codecs in Parser
    - Add all encoding codecs to be considered when read files.

--- shorten_paper/file_operations_utils.py
+++ shorten_paper/file_operations_utils.py
@@ -20,6 +20,104 @@ from shorten_paper.config import Config
 logger = Logger()
 CFG = Config()
 
+ENCODINGS = ['utf_8',
+             'ascii',
+             'big5',
+             'big5hkscs',
+             'cp037',
+             'cp273',
+             'cp424',
+             'cp437',
+             'cp500',
+             'cp720',
+             'cp737',
+             'cp775',
+             'cp850',
+             'cp852',
+             'cp855',
+             'cp856',
+             'cp857',
+             'cp858',
+             'cp860',
+             'cp861',
+             'cp862',
+             'cp863',
+             'cp864',
+             'cp865',
+             'cp866',
+             'cp869',
+             'cp874',
+             'cp875',
+             'cp932',
+             'cp949',
+             'cp950',
+             'cp1006',
+             'cp1026',
+             'cp1125',
+             'cp1140',
+             'cp1250',
+             'cp1251',
+             'cp1252',
+             'cp1253',
+             'cp1254',
+             'cp1255',
+             'cp1256',
+             'cp1257',
+             'cp1258',
+             'euc_jp',
+             'euc_jis_2004',
+             'euc_jisx0213',
+             'euc_kr',
+             'gb2312',
+             'gbk',
+             'gb18030',
+             'hz',
+             'iso2022_jp',
+             'iso2022_jp_1',
+             'iso2022_jp_2',
+             'iso2022_jp_2004',
+             'iso2022_jp_3',
+             'iso2022_jp_ext',
+             'iso2022_kr',
+             'latin_1',
+             'iso8859_2',
+             'iso8859_3',
+             'iso8859_4',
+             'iso8859_5',
+             'iso8859_6',
+             'iso8859_7',
+             'iso8859_8',
+             'iso8859_9',
+             'iso8859_10',
+             'iso8859_11',
+             'iso8859_13',
+             'iso8859_14',
+             'iso8859_15',
+             'iso8859_16',
+             'johab',
+             'koi8_r',
+             'koi8_t',
+             'koi8_u',
+             'kz1048',
+             'mac_cyrillic',
+             'mac_greek',
+             'mac_iceland',
+             'mac_latin2',
+             'mac_roman',
+             'mac_turkish',
+             'ptcp154',
+             'shift_jis',
+             'shift_jis_2004',
+             'shift_jisx0213',
+             'utf_32',
+             'utf_32_be',
+             'utf_32_le',
+             'utf_16',
+             'utf_16_be',
+             'utf_16_le',
+             'utf_7',
+             'utf_8_sig']
+
 
 class ParserStrategy:
     def read(self, file_path):
@@ -29,9 +127,15 @@ class ParserStrategy:
 # Basic text file reading
 class TXTParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            text = f.read()
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    text = f.read()
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("TXTParser Read Error.")
 
 
 # Reading text from binary file using pdf parser
@@ -57,51 +161,87 @@ class DOCXParser(ParserStrategy):
 # Reading as dictionary and returning string format
 class JSONParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            data = json.load(f)
-            text = str(data)
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    data = json.load(f)
+                    text = str(data)
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("JSONParser Read Error.")
 
 
 class XMLParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            soup = BeautifulSoup(f, "xml")
-            text = soup.get_text()
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    soup = BeautifulSoup(f, "xml")
+                    text = soup.get_text()
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("XMLParser Read Error.")
 
 
 # Reading as dictionary and returning string format
 class YAMLParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            data = yaml.load(f, Loader=yaml.FullLoader)
-            text = str(data)
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    data = yaml.load(f, Loader=yaml.FullLoader)
+                    text = str(data)
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("YAMLParser Read Error.")
 
 
 class HTMLParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            soup = BeautifulSoup(f, "html.parser")
-            text = soup.get_text()
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    soup = BeautifulSoup(f, "html.parser")
+                    text = soup.get_text()
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("HTMLParser Read Error.")
 
 
 class MarkdownParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            html = markdown.markdown(f.read())
-            text = "".join(BeautifulSoup(html, "html.parser").findAll(string=True))
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    html = markdown.markdown(f.read())
+                    text = "".join(BeautifulSoup(html, "html.parser").findAll(string=True))
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("MarkdownParser Read Error.")
 
 
 class LaTeXParser(ParserStrategy):
     def read(self, file_path):
-        with open(file_path, "r") as f:
-            latex = f.read()
-        text = LatexNodes2Text().latex_to_text(latex)
-        return text
+        for encoding in ENCODINGS:
+            try:
+                with open(file_path, "r", encoding=encoding) as f:
+                    latex = f.read()
+                    text = LatexNodes2Text().latex_to_text(latex)
+                    logger.typewriter_log(f"Encoding:", Fore.CYAN, encoding)
+                return text
+            except UnicodeDecodeError:
+                continue
+        raise IOError("LaTeXParser Read Error.")
 
 
 class FileContext:
